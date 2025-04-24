dd746f39d35d10fc22b1b9be9b1176c10a0dbc31
MalinduTharaka <malindu999555@gmail.com>
2025-04-24 17:03:31 +0530

    logo fix

--- resources/views/layouts/left-nav.blade.php
+++ resources/views/layouts/left-nav.blade.php
@@ -3,20 +3,22 @@
     <!-- Brand Logo Light -->
     <a href="index.html" class="logo logo-light">
         <span class="logo-lg">
-            <img src="{{ asset('assets/images/logo.png')}}" alt="logo">
+            <img src="{{ asset('logos/wishwaads.jpg')}}" alt="logo" style="height: 60px; width: auto;">
+
         </span>
         <span class="logo-sm">
-            <img src="{{ asset('assets/images/logo-sm.png')}}" alt="small logo">
+            <img src="{{ asset('logos/wishwaads.jpg')}}" alt="small logo">
         </span>
     </a>
 
     <!-- Brand Logo Dark -->
     <a href="index.html" class="logo logo-dark">
         <span class="logo-lg">
-            <img src="{{ asset('assets/images/logo-dark.png')}}" alt="dark logo">
+            <img src="{{ asset('logos/wishwaads.jpg')}}" alt="logo" style="height: 60px; width: auto;">
+
         </span>
         <span class="logo-sm">
-            <img src="{{ asset('assets/images/logo-sm.png')}}" alt="small logo">
+            <img src="{{ asset('logos/wishwaads.jpg')}}" alt="small logo">
         </span>
     </a>
 
